a5fe17d993d7ebd039ed83b363809894a6bd4ea6
Iorgen <w.drik@outlook.com>
2018-12-12 19:07:33 -0200

    New models!

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
-const User = require('../models/User');
-
 const router = express.Router();
 
-router.get('/', authMiddleware, (req, res) => {
+router.use(authMiddleware)
 
+router.get('/', (req, res) => {
   res.send({ id: req.userId });
 })
 
